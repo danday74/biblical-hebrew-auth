8e68b1f7bac6b99c2414eded3ba917dd6128c132
Daniel Lewis <daniellewis777@gmail.com>
2019-04-08 18:26:27 +0100

    no auth whitelist added

--- middlewares/3-request-gatekeeper.js
+++ middlewares/3-request-gatekeeper.js
@@ -1,10 +1,15 @@
+const {find} = require('lodash')
 const jwt = require('jsonwebtoken')
 const config = require('../authServer.config')
+const NO_AUTH_REQUIRED_URLS = require('./no-auth-required-urls')
 
 const middleware = (req, res, next) => {
-  if (req.url === '/login' && req.method === 'POST') {
-    return next()
-  }
+
+  const noAuthRequiredUrl = find(NO_AUTH_REQUIRED_URLS, noAuthRequiredUrl => {
+    return req.url.startsWith(noAuthRequiredUrl.url) && noAuthRequiredUrl.method === req.method
+  })
+  if (noAuthRequiredUrl) return next()
+
   /* istanbul ignore next */
   if (req.url === '/mock-validate-user' && req.method === 'POST') {
     return next()
